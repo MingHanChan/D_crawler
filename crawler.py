2a7c2c464c075091341373231470c45cf4129a16
Ming-Han Chan <47624400+MingHanChan@users.noreply.github.com>
2019-10-08 15:24:24 +0800

    v_1.0

--- crawler.py
+++ crawler.py
@@ -1,190 +1,80 @@
-import requests
-import json
 import os
-import time
 import sys
-import threading
-import queue
+import time
+import datetime
+import argparse
 
-url_API_FORUMS = 'https://www.dcard.tw/_api/forums/'
-url_API_FILTER = '/posts?popular=false'
-url_API_POSTS = 'https://www.dcard.tw/_api/posts/'
+# customized module
+from module import Dcard
+from module import Thread
 
-# argv
-debug = '--debug' in sys.argv
+# constant
+__version__ = '1.0'
+__description__ = 'dcard-crawler - a simple crawler for dcard.tw'
+__epilog__ = 'Report bugs to <cjyeh@cs.nctu.edu.tw>'
+PATH = os.path.dirname(os.path.abspath(__file__)) + '/data/'
 
-# globle var
-q = queue.Queue()
+def parse_argv():
+	# init
+	parser = argparse.ArgumentParser(
+		description=__description__,
+		epilog=__epilog__
+	)
 
-def next():
-	while q.qsize() > 0:
-		job = q.get()
-		job.run()
+	# add argument
+	parser.add_argument('forum')
+	parser.add_argument(
+		'-t', '--threads_num', 
+		action='store', 
+		help='Number of threads, default = 4', 
+		type=int, 
+		default=4
+	)
+	parser.add_argument(
+		'--debug', 
+		action='store_true', 
+		help='Show debug information', 
+		default=False
+	)
+	parser.add_argument(
+		'-v', '-V', '--version', 
+		action='version', 
+		help='Print program version', 
+		version='v{}'.format(__version__)
+	)
 
-class Crawler:
-	def __init__(self, id):
-		self.id = id
-		self.path = str(os.path.abspath(os.path.join(__file__, os.pardir)))
+	results = parser.parse_args()
+	
+	return {
+		'forum': results.forum,
+		'threads_num': results.threads_num,
+		'debug': results.debug
+	}
 
-	def request_post(self, post_id):
-		try:
-			__request = requests.get(url_API_POSTS + str(post_id))
-			__response = json.loads(__request.text)
-		except:
-			if debug:
-				print('Error: ' + str(post_id) + ' request_post')
-			return
-		return __response
+def main():
+	# init
+	Dcard.load_db()
 
-	def request_comment(self, post_id):
-		try:
-			__request = requests.get(url_API_POSTS + str(post_id) + '/comments')
-			__response = json.loads(__request.text)
-		except:
-			if debug:
-				print('Error: ' + str(post_id) + ' request_comment')
-			return
-		return __response
+	# var
+	args = parse_argv()
 
-	def save_file(self, var):
-		try:
-			path = self.path + '/data/' + var['posts']['forumAlias'] + '/'
+	for i in Dcard.db:
+		if args['forum'] and i['forumAlias'] != args['forum']:
+			continue
 
-			# create folder
-			if not os.path.exists(path):
-				os.makedirs(path)
+		# init
+		Dcard.init_forum(i)
 
-			file_name = str(var['posts']['id']) + '.json'
-			f = open(path + file_name, 'w', encoding='UTF-8')
-			f.write(json.dumps(var))
-			f.close()
-		except:
-			if debug:
-				print('Error: ' + str(var['posts']['id']) + ' save_file')
-			return
+		if not os.path.exists(PATH + i['forumAlias']):
+			os.makedirs(PATH + i['forumAlias'])
 
-	def run(self):
-		temp = {
-			"posts": self.request_post(self.id),
-			"comments": self.request_comment(self.id)
-		}
-		self.save_file(temp)
-		if debug:
-			print(self.id)
+		# threading
+		Thread.run(args)
 
-class Dcard:
-	def __init__(self):
-		# var
-		self.path = str(os.path.abspath(os.path.join(__file__, os.pardir)))
+		# update
+		i['latest'] = max([int(j) for j in os.listdir(PATH + i['forumAlias'])])
+		i['updateAt'] = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+		Dcard.save_db()
 
-		# para
-		try:
-			print('Loading paraments...')
-			with open(self.path + '/paraments.json', 'r') as file:
-				self.para = json.load(file)
-		except:
-			print('Creating paraments...')
-			# var
-			self.para = []
-
-			# request forums list
-			try:
-				print('Requesting forum list...')
-				__request = requests.get(url_API_FORUMS)
-				__response = json.loads(__request.text)
-			except:
-				print('Error: requests.get() failed')
-
-			# init
-			for x in __response:
-				temp = {
-					"alias": x['alias'],
-					"updatedAt": "null",
-					"max": 0,
-					"min": 0
-				}
-				self.para.append(temp)
-
-	def request_post_list(self, var, index):
-		try:
-			__request = requests.get(url_API_FORUMS + var['alias'] + url_API_FILTER + '&before=' + str(index))
-			__response = json.loads(__request.text)
-			return __response
-		except:
-			if debug:
-				print('Error: ' + var['alias'] + ' request before ' + str(index))
-			return
-
-	def update_para(self):
-		with open(self.path + '/paraments.json', 'w') as file:
-			json.dump(self.para, file)
-
-	def run_forum(self, var):
-		# request lastest post id
-		try:
-			__request = requests.get(url_API_FORUMS + var['alias'] + url_API_FILTER)
-			__response = json.loads(__request.text)
-			index = __response[0]['id']
-		except:
-			if debug:
-				print('Error: ' + var['alias'] + ' list')
-			return
-
-		while index > var['max']:
-			# init
-			post_list = self.request_post_list(var, index)
-
-			try:
-				if len(post_list) == 0:
-					# first
-					if var['min'] == 0:
-						var['min'] = index
-					break
-			except:
-				continue
-
-			# init
-			for i in post_list:
-				try:
-					# update max
-					index = i['id']
-
-					# done
-					if i['id'] <= var['max']:
-						break
-
-					q.put(Crawler(i['id']))
-				except:
-					print('Error: init')
-
-			# create threads 
-			t1 = threading.Thread(target=next, name='T1')
-			t2 = threading.Thread(target=next, name='T2')
-			t3 = threading.Thread(target=next, name='T3')
-			t4 = threading.Thread(target=next, name='T4')
-
-			t1.start()
-			t2.start()
-			t3.start()
-			t4.start()
-
-			while t1.is_alive() or t2.is_alive() or t3.is_alive() or t4.is_alive():
-				time.sleep(1)
-
-		# update para
-		temp = self.para.index(var)
-		if self.para[temp]['max'] < __response[0]['id']:
-			self.para[temp]['max'] = __response[0]['id']
-		self.update_para()
-
-	def run_all(self):
-		for x in self.para:
-			# console
-			print('ForumAlias: ' + x['alias'])
-			print('Percentage: ' + str(self.para.index(x) + 1) + '/' + str(len(self.para)))
-			self.run_forum(x)
-
-while 1:
-	dcard = Dcard()
-	dcard.run_all()
-	time.sleep(600) 
+if __name__ == '__main__':
+	main()
